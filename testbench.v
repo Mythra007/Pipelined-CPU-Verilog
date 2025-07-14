c3b7504bc47986fa5f49ce4742c75bcb7cd683b4
Mythra007 <d.mythra@iitg.ac.in>
2025-07-14 22:41:24 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -3,17 +3,27 @@
 module testbench;
 
     reg clk;
+    wire [7:0] pc_out, instruction_out, alu_result_out, out_val_out;
+    wire [7:0] reg_rs_out, reg_rt_out;
+    wire zero_flag_out, neg_flag_out;
 
-    // Instantiate the top module (match the name and port exactly)
-    cpu_top CPU (.clk(clk));
+    cpu_top CPU (
+        .clk(clk),
+        .pc_out(pc_out),
+        .instruction_out(instruction_out),
+        .alu_result_out(alu_result_out),
+        .out_val_out(out_val_out),
+        .reg_rs_out(reg_rs_out),
+        .reg_rt_out(reg_rt_out),
+        .zero_flag_out(zero_flag_out),
+        .neg_flag_out(neg_flag_out)
+    );
 
-    // Clock generation
     initial begin
         clk = 0;
         forever #5 clk = ~clk;
     end
 
-    // Stop the simulation after 200ns
     initial begin
         #200 $finish;
     end
